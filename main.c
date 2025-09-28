e21c8c3a25f7b0371e8f0ba485cf8a9db0d4b97d
akshaybachhu-53 <akshaybachhu16@gmail.com>
2025-09-28 15:16:01 +0530

    Pthreads

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<pthread.h>
+
+void* foo(void* args){
+    int id = *((int*)args);
+    printf("Hello Akshay this is your ID: %d\n", id);
+    return NULL;
+}
+int main(){
+    pthread_t thread1;
+    int id = 53;
+    pthread_create(&thread1, NULL, foo, &id);
+    pthread_join(thread1, NULL);
+    return 0;
+}
